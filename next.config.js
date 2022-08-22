59546513e08f4567e053cf5e5b90171ba6c79553
Fabio Maienschein <fabio.maienschein@gmail.com>
2022-08-22 19:23:42 +0200

    Add netlify cms, test out in home site

--- next.config.js
+++ next.config.js
@@ -10,6 +10,14 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'tailwindui.com', 'placekitten.com'],
   },
+  webpack: (cfg) => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: 'frontmatter-markdown-loader',
+      options: { mode: ['react-component'] },
+    })
+    return cfg
+  },
 }
 
 module.exports = nextConfig
